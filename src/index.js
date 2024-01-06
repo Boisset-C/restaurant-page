7d8c41cf980440e11aed80636d48191167c3bb8e
cosme boisset <cosmeboisset@cosmes-MacBook-Pro.local>
2024-01-06 17:57:21 -0600

    Project initialized. Wepback is working.

---
+++ src/index.js
@@ -0,0 +1,1 @@
+// alert("Webpack is up and running!!!");
---
+++ src/pages/homepage.js
@@ -0,0 +1,18 @@
+/* <h1>Restaurant Title</h1>
+			<p>
+				Blah blah something about Restaurants.Blah blah something about
+				RestaurantsBlah blah something about RestaurantsBlah blah
+				something about RestaurantsBlah blah something about
+				RestaurantsBlah blah something about RestaurantsBlah blah
+				something about RestaurantsBlah blah something about
+				RestaurantsBlah blah something about RestaurantsBlah blah
+				something about RestaurantsBlah blah something about
+				RestaurantsBlah blah something about Restaurants
+			</p>
+			<img
+				src="../assets/placeholder.jpg"
+				alt=""
+				width="400px"
+				height="400px"
+			/>
+*/
